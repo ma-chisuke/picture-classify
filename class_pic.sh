fe759aa92c31557f605dcb9733df5a77d9cd17f5
ma.chisuke <ma.chisuke0@gmail.com>
2020-06-03 00:01:12 +0900

    Initial commit

---
+++ class_pic.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -u
+
+DISTINATION=dist
+
+function classification() {
+  local _f=$1
+  local _subDir
+  _subDir=$(exiftool "${_f}" | grep "File Modification Date/Time" | sed 's/.*: //g' | awk '{ print $1}' | tr : _)
+
+  mkdir -p "./${DISTINATION}/${_subDir}"
+  if ! mv -v "${_f}" "./${DISTINATION}/${_subDir}/" ;then
+    echo "Error. Failed move file."
+    exit 2
+  fi
+}
+
+while read -r l ;do
+  [[ -z $l ]] && exit
+  classification "$l"
+done <<_EOL_
+$(find . \
+  -type f \
+  -and ! -path "./${DISTINATION}/*" \
+  -and ! -path "./.vscode/*" \
+  -and ! -name "*.sh" \
+  -and ! -name ".*")
+_EOL_
